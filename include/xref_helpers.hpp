9dfbf2523047cc5df8196496720b18b1e64232f4
Millzy <millzyyt08@gmail.com>
2024-05-19 16:45:29 +0100

    find nth port

--- include/xref_helpers.hpp
+++ include/xref_helpers.hpp
@@ -2,12 +2,71 @@
 #define GLUON_XREF_HELPERS_HPP_
 
 #include <cstdint>
+#include <optional>
+
+#include "gluon_config.hpp"
+#include "gluon_logging.hpp"
+#include "capstone/capstone.h"
 
 namespace Gluon::XrefHelpers {
+    GLUON_API void initialiseCapstone();
+    GLUON_API csh getCapstoneHandle();
+
     template<uint32_t  nToRetOn, auto match, auto skip, int retCount = -1, size_t szBytes = 4096>
     requires(nToRetOn >= 1)
     auto findNth(const uint32_t *address) {
+        cs_insn *insn = cs_malloc(getCapstoneHandle());
+        auto ptr = reinterpret_cast<uint64_t>(address);
+        auto instructions = reinterpret_cast<const uint8_t *>(address);
 
+        int rCount = retCount;
+        uint32_t nCalls = nToRetOn;
+        size_t size = szBytes;
+
+        while (size > 0) {
+            bool res = cs_disasm_iter(getCapstoneHandle(), &instructions, &size, &ptr, insn);
+            if (res) {
+                if (insn->id == X86_INS_RET) {
+                    if (rCount == 0) {
+                        cs_free(insn, 1);
+                        Gluon::Logging::Logger::warn("Could not find: {} call at: {} within {} rets! Found all of the rets first!", nToRetOn, (void*)ptr, retCount);
+                        return (decltype(match(insn)))std::nullopt;
+                    }
+                    rCount--;
+                }
+                else {
+                    auto testRes = match(insn);
+                    if (testRes) {
+                        if (nCalls == 1) {
+                            cs_free(insn, 1);
+                            return testRes;
+                        }
+                        else {
+                            nCalls--;
+                        }
+                    }
+                    else if (skip(insn)) {
+                        if (nCalls == 1) {
+                            std::string name(insn->mnemonic);
+                            cs_free(insn, 1);
+                            Gluon::Logging::Logger::warn("Found: {} match, at: {} within: {} rets, but the result was a {}! Cannot compute destination address!", nToRetOn, (void*)ptr, retCount, name);
+                            return (decltype(match(insn)))std::nullopt;
+                        }
+                        else {
+                            nCalls--;
+                        }
+                    }
+                }
+                // Other instructions are silently ignored.
+            }
+            else {
+                // This is where invalid instructions would be skipped.
+                // Unfortunately, CISC architectures make this annoying.
+                // Therefore, I have elected to ignore it and hope nothing bad happens.
+            }
+        }
+        cs_free(insn, 1);
+        return (decltype(match(insn)))std::nullopt;
     }
 }
 
--- src/xref_helpers.cpp
+++ src/xref_helpers.cpp
@@ -1,1 +1,24 @@
 #include "xref_helpers.hpp"
+
+#include "gluon_logging.hpp"
+#include "capstone/capstone.h"
+
+namespace Gluon::XrefHelpers {
+    static csh capstoneHandle;
+    static bool valid;
+
+    void inititialiseCapstone() {
+        cs_err csError = cs_open(CS_ARCH_X86, CS_MODE_64, &capstoneHandle);
+        cs_option(capstoneHandle, CS_OPT_DETAIL, 1);
+        if (csError) {
+            Gluon::Logging::Logger::error("Capstone initialisation failed! {0}", static_cast<int>(csError));
+            // TODO: Abort
+        }
+        Gluon::Logging::Logger::info("Capstone initialised!");
+        valid = true;
+    }
+
+    csh getCapstoneHandle() {
+        return capstoneHandle;
+    }
+}
