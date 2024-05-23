c423393013b96ca1f034d528196bacfe302c5334
Millzy <millzyyt08@gmail.com>
2024-05-23 19:17:53 +0100

    default classes

--- include/il2cpp_internals.hpp
+++ include/il2cpp_internals.hpp
@@ -5,10 +5,29 @@
 #include "il2cpp-class-internals.h"
 
 namespace Gluon {
+    struct BuiltinClasses {
+        Il2CppClass *booleanClass;
+        Il2CppClass *byteClass;
+        Il2CppClass *sByteClass;
+        Il2CppClass *charClass;
+        Il2CppClass *singleClass;
+        Il2CppClass *doubleClass;
+        Il2CppClass *int16Class;
+        Il2CppClass *uint16Class;
+        Il2CppClass *int32Class;
+        Il2CppClass *uint32Class;
+        Il2CppClass *int64Class;
+        Il2CppClass *uint64Class;
+        Il2CppClass *objectClass;
+        Il2CppClass *stringClass;
+        Il2CppClass *voidClass;
+        Il2CppClass *typeClass;
+    };
+
     class GLUON_API Il2CppInternals {
     public:
         static bool initialised;
-        static Il2CppDefaults defaults;
+        static BuiltinClasses builtinClasses;
 
         /**
          * @warning DO NOT CALL BEFORE IL2CPP HAS BEEN INITIALISED.
--- src/il2cpp_internals.cpp
+++ src/il2cpp_internals.cpp
@@ -3,12 +3,14 @@
 #include "il2cpp_internals.hpp"
 
 #include "il2cpp_functions.hpp"
-#include "xref_helpers.hpp"
+#include "assemblies.hpp"
+#include "classes.hpp"
 #include "il2cpp-class-internals.h"
+
+#define DEFAULTS_INIT(name, namespaze, klass, ...) defaults.name = Gluon::Classes::findClassFromName(namespaze, name)
 
 namespace Gluon {
     bool Il2CppInternals::initialised = false;
-    Il2CppDefaults Il2CppInternals::defaults;
 
     void Il2CppInternals::initialise() {
         if (initialised) {
@@ -16,8 +18,24 @@ namespace Gluon {
         }
         initialised = true;
 
-        std::memset(&defaults, 0, sizeof(Il2CppDefaults));
-        
+        std::memset(&builtinClasses, 0, sizeof(BuiltinClasses));
+        builtinClasses.booleanClass = Gluon::Classes::findClassFromName("System", "Boolean");
+        builtinClasses.byteClass = Gluon::Classes::findClassFromName("System", "Byte");
+        builtinClasses.sByteClass = Gluon::Classes::findClassFromName("System", "SByte");
+        builtinClasses.charClass = Gluon::Classes::findClassFromName("System", "Char");
+        builtinClasses.singleClass = Gluon::Classes::findClassFromName("System", "Single");
+        builtinClasses.doubleClass = Gluon::Classes::findClassFromName("System", "Double");
+        builtinClasses.int16Class = Gluon::Classes::findClassFromName("System", "Int16");
+        builtinClasses.uint16Class = Gluon::Classes::findClassFromName("System", "UInt16");
+        builtinClasses.int32Class = Gluon::Classes::findClassFromName("System", "Int32");
+        builtinClasses.uint32Class = Gluon::Classes::findClassFromName("System", "UInt32");
+        builtinClasses.int64Class = Gluon::Classes::findClassFromName("System", "Int64");
+        builtinClasses.uint64Class = Gluon::Classes::findClassFromName("System", "UInt32");
+        builtinClasses.objectClass = Gluon::Classes::findClassFromName("System", "Object");
+        builtinClasses.stringClass = Gluon::Classes::findClassFromName("System", "String");
+        builtinClasses.voidClass = Gluon::Classes::findClassFromName("System", "Void");
+        builtinClasses.typeClass = Gluon::Classes::findClassFromName("System", "Type");
+
         // TODO: defaults type map, avoiding an xref trace because pain
     }
 }
