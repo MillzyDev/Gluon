a2cf9d666280d60fca785d3c007ae6774f0ce32b
Millzy <millzyyt08@gmail.com>
2024-06-15 19:00:10 +0100

    find method unsafe

--- src/methods.cpp
+++ src/methods.cpp
@@ -1,23 +1,31 @@
+#include <map>
+#include <mutex>
+
 #include "methods.hpp"
 
+#include "classes.hpp"
 #include "gc.hpp"
+#include "gluon_logging.hpp"
+#include "hashing.hpp"
+#include "il2cpp_functions.hpp"
+
 #include "il2cpp-object-internals.h"
 
 namespace Gluon::Methods {
     Il2CppObject *createManual(const Il2CppClass *klass) noexcept {
         if (!klass) {
-            // cannot create manual on null class
+            Gluon::Logging::Logger::error("Cannot create a manual object on a null class!");
             return nullptr;
         }
 
         if (!klass->initialized) {
-            // cannot create an object from uninitalised klass
+            Gluon::Logging::Logger::error("Cannot create an object that does not have an initialised class: {:x}", reinterpret_cast<uintptr_t>(klass));
             return nullptr;
         }
 
         auto *object = reinterpret_cast<Il2CppObject *>(Gluon::Gc::gcAllocSpecific(klass->instance_size));
         if (!object) {
-            // failed to allocate
+            Gluon::Logging::Logger::error("Failed to allocate GC specific area for instance size: {}", klass->instance_size);
             return nullptr;
         }
 
@@ -35,4 +43,45 @@ namespace Gluon::Methods {
     Il2CppObject *createManualThrow(const Il2CppClass *klass) {
 
     }
+
+    const MethodInfo *findMethodUnsafe(const Il2CppClass *klass, std::string_view methodName, int argsCount) {
+        Gluon::Il2CppFunctions::initialise();
+
+        if (!klass) {
+            return nullptr;
+        }
+
+        // cache
+        static std::unordered_map<std::pair<const Il2CppClass *, std::pair<std::string, uint8_t>>, const MethodInfo *, Gluon::Hashing::HashPair3> unsafeMethodsCache;
+        static std::mutex unsafeMethodsCacheMutex;
+
+        // check cache
+        auto innerPair = std::pair<std::string, uint8_t>(methodName, argsCount);
+        auto key = std::pair<const Il2CppClass *, decltype(innerPair)>(klass, innerPair);
+        unsafeMethodsCacheMutex.lock();
+
+        auto it = unsafeMethodsCache.find(key);
+        if (it != unsafeMethodsCache.end()) {
+            unsafeMethodsCacheMutex.unlock();
+            return it->second;
+        }
+        unsafeMethodsCacheMutex.unlock();
+
+        // try get from IL2CPP
+        const MethodInfo *methodInfo = Gluon::Il2CppFunctions::class_get_method_from_name(klass, methodName.data(), argsCount);
+        if (!methodInfo) {
+            Gluon::Logging::Logger::error("Could not fund method {} with {} parameters in class '{}'.", methodName.data(),
+                                          argsCount, Gluon::Classes::getClassStandardName(klass).c_str());
+            // TODO: LogMethods (requires implementation)
+            return nullptr;
+        }
+
+        unsafeMethodsCacheMutex.lock();
+        unsafeMethodsCache.emplace(key, methodInfo);
+        unsafeMethodsCacheMutex.unlock();
+
+        return methodInfo;
+    }
+
+
 }
