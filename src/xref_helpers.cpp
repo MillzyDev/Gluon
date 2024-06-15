df529f44c023a59cc39ba47740c6a80fcfac3004
Millzy <millzyyt08@gmail.com>
2024-06-15 18:57:45 +0100

    abort on csError

--- src/xref_helpers.cpp
+++ src/xref_helpers.cpp
@@ -3,6 +3,7 @@
 
 #include "xref_helpers.hpp"
 
+#include "abortion.hpp"
 #include "gluon_logging.hpp"
 #include "capstone/capstone.h"
 
@@ -15,7 +16,7 @@ namespace Gluon::XrefHelpers {
         cs_option(capstoneHandle, CS_OPT_DETAIL, 1);
         if (csError) {
             Gluon::Logging::Logger::error("Capstone initialisation failed! {0}", static_cast<int>(csError));
-            // TODO: Abort
+            SAFE_ABORT();
         }
         Gluon::Logging::Logger::info("Capstone initialised!");
         valid = true;
