ac6a0b3ae741997e4cb424869d2f257e4c557c92
Millz <millzyyt08@gmail.com>
2025-05-10 17:54:39 +0100

    hidden attribute

--- include/gluon_config.hpp
+++ include/gluon_config.hpp
@@ -2,9 +2,11 @@
 #define GLUON_GLUON_CONFIG_HPP
 
 #ifdef GLUON_EXPORT
-#define GLUON_API __declspec(dllexport)
+#define GLUON_API __attribute__((visibility("default")))
 #else
 #define GLUON_API __declspec(dllimport)
 #endif
+
+#define GLUON_HIDDEN __attribute__((visibility("hidden")))
 
 #endif //GLUON_GLUON_CONFIG_HPP
