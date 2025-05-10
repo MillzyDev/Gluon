3c0ba2e0b1b00645df513448a2fd5d0348e40f41
Millz <millzyyt08@gmail.com>
2025-05-10 12:28:49 +0100

    new unity version to test on

--- unity.cmake
+++ unity.cmake
@@ -1,5 +1,5 @@
-set(UNITY_DIRECTORY "C:\\Program Files\\Unity\\Hub\\Editor\\2021.3.5f1")
+set(UNITY_DIRECTORY "C:\\Program Files\\Unity\\Hub\\Editor\\2021.3.16f1")
 
 set(UNITY_MAJOR "2021")
 set(UNITY_MINOR "3")
-set(UNITY_PATCH "5f1")
+set(UNITY_PATCH "16f1")
