40d8d50c1e8f5fc0f842e7e46ca2a3a3aefa3f9d
Millz <millzyyt08@gmail.com>
2025-05-12 19:52:48 +0100

    find method done kill me now

--- include/methods.hpp
+++ include/methods.hpp
@@ -1,9 +1,12 @@
 #ifndef GLUON_METHODS_HPP_
 #define GLUON_METHODS_HPP_
 
-#include <classes.hpp>
 #include <cstdint>
+#include <optional>
+#include <ranges>
+#include <string>
 #include <string_view>
+#include <vector>
 
 #include "il2cpp-api.h"
 
@@ -11,9 +14,151 @@
 #include "gluon_config.hpp"
 
 namespace Gluon::Methods {
+    struct FindMethodInfo {
+        Il2CppClass *klass = nullptr;
+        std::string name;
+        std::vector<const Il2CppClass *const> genericTypes;
+        std::vector<const Il2CppType *const> argTypes;
+
+        constexpr FindMethodInfo() = delete;
+        constexpr FindMethodInfo(FindMethodInfo &&) = default;
+        constexpr FindMethodInfo(FindMethodInfo const &) = delete;
+        constexpr FindMethodInfo(Il2CppClass *klass, std::string_view name,
+            std::span<const Il2CppClass *const> genericTypes, std::span<const Il2CppType *const> argTypes)
+                : klass(klass), name(name), genericTypes(genericTypes.begin(), genericTypes.end()),
+                  argTypes(argTypes.begin(), argTypes.end()) {}
+
+        bool operator==(const FindMethodInfo & other) const {
+            if (this->klass != other.klass) {
+                return false;
+            }
+
+            if (this->name != other.name) {
+                return false;
+            }
+
+            const bool isArgTypesEqual =
+                this->argTypes.size() == other.argTypes.size() &&
+                std::ranges::equal(this->argTypes, other.argTypes);
+            if (!isArgTypesEqual) {
+                return false;
+            }
+
+            const bool isGenericTypesEqual =
+                this->genericTypes.size() == other.genericTypes.size() &&
+                std::ranges::equal(this->genericTypes, other.genericTypes);
+            if (!isGenericTypesEqual) {
+                return false;
+            }
+
+            return true;
+        }
+
+        bool operator!=(FindMethodInfo const &) const = default;
+    };
+
+    /**
+     * @brief Finds the first MethodInfo* described by the given Il2CppClass*, method name, and argument count.
+     * Throws an Il2CppUtilException when: klass is null, or the method could not be found.
+     * @return The found MethodInfo*
+     * @param klass The Il2CppClass* to search for the method
+     * @param methodName The il2cpp name of the method to find
+     * @param argsCount The number of arguments to match (or -1 to not match at all)
+     */
     GLUON_API const MethodInfo *findMethodUnsafe(const Il2CppClass *klass, std::string_view methodName, int argsCount);
+    /**
+     * @brief Find the first MethodInfo* of the class described by the namespace and className, described by the methodName, and argument count.
+     * Throws an Il2CppUtilException when: the Il2CppClass* could not be found, or the method could not be found.
+     * @return The found MethodInfo*
+     * @param namespaze The namespace in which to search for the class
+     * @param className The il2cpp name of the class to find
+     * @param methodName The il2cpp name of the method to find
+     * @param argsCount The number of arguments to match (or -1 to not match at all)
+     */
     GLUON_API const MethodInfo *findMethodUnsafe(std::string_view namespaze, std::string_view className, std::string_view methodName, int argsCount);
+    /**
+     * @brief Find the first MethodInfo* on the given instance, described by the methodName, and argument count.
+     * Throws an Il2CppUtilException when: instance is null, the Il2CppClass* could not be loaded, or the method could not be found.
+     * @return The found MethodInfo*
+     * @param instance The Il2CppObject* to search for the method
+     * @param methodName The il2cpp name of the method to find
+     * @param argsCount The number of arguments to match (or -1 to not match at all)
+     */
     GLUON_API const MethodInfo *findMethodUnsafe(Il2CppObject *instance, std::string_view methodName, int argsCount);
+
+    /**
+     * Attempts to look for a method that best matches given the FindMethodInfo data
+     * if no method is found, returns null
+     * Look at methods.cpp for more details on how this resolution takes place
+     */
+    GLUON_API const MethodInfo *findMethod(FindMethodInfo &&info);
+
+    // helper constructor
+    template<typename T, typename TGenerics, typename TArgs>
+    requires(!std::is_convertible_v<T, std::string_view> && std::is_constructible_v<std::span<const Il2CppClass *const>, TGenerics> && std::is_constructible_v<std::span<const Il2CppType *const>, TArgs>)
+    const MethodInfo *findMethod(T &&instanceOrClass, std::string_view methodName, TGenerics &&genericTypes, TArgs&&argTypes) {
+        Il2CppClass *klass = Gluon::Classes::extractClass(std::forward<T>(instanceOrClass));
+        const auto genericTypesSpan = std::span<const Il2CppClass *const>(std::forward<TGenerics>(genericTypes));
+        const auto argTypesSpan = std::span<const Il2CppType *const>(std::forward<TArgs>(argTypes));
+        return findMethod(FindMethodInfo(klass, methodName, genericTypesSpan, argTypesSpan));
+    }
+
+    // no generic args
+    template <typename T, typename TArgs>
+    requires(!std::is_constructible_v<T, std::string_view>)
+    const MethodInfo *findMethod(T &&instanceOrClass, std::string_view methodName, TArgs &&argTypes) {
+        return findMethod(std::forward<T>(instanceOrClass), methodName, std::forward<TArgs>(argTypes));
+    }
+
+    // no args
+    template <typename T>
+    requires(!std::is_constructible_v<T, std::string_view>)
+    const MethodInfo *findMethod(T &&instanceOrClass, std::string_view methodName) {
+        return findMethod(std::forward<T>(instanceOrClass), methodName, std::span<const Il2CppClass *const>(), std::span<const Il2CppType *const>());
+    }
+
+    // generic and array args
+    template <typename TGenerics, typename TArgs>
+    const MethodInfo *findMethod(const std::string_view namespaze, const std::string_view className, std::string_view methodName, TGenerics &&genericTypes, TArgs &&argTypes) {
+        Il2CppClass *klass = Gluon::Classes::getClassFromName(namespaze, className);
+        return findMethod(klass, methodName, std::forward<TGenerics>(genericTypes), std::forward<TArgs>(argTypes));
+    }
+
+    // no generic args
+    template <typename TArgs>
+    const MethodInfo *findMethod(const std::string_view namespaze, const std::string_view className, std::string_view methodName, TArgs &&argTypes) {
+        Il2CppClass *klass = Gluon::Classes::getClassFromName(namespaze, className);
+        return findMethod(klass, methodName, std::span<const Il2CppClass *const>(), std::forward<TArgs>(argTypes));
+    }
+
+    // no args
+    inline const MethodInfo *findMethod(const std::string_view namespaze, const std::string_view className, std::string_view methodName) {
+        Il2CppClass *klass = Gluon::Classes::getClassFromName(namespaze, className);
+        return findMethod(klass, methodName, std::span<const Il2CppClass *const>(), std::span<const Il2CppType *const>());
+    }
+
+    bool isConvertibleFrom(const Il2CppType *to, const Il2CppType *from, bool asArgs);
+
+    inline const Il2CppGenericContainer *getGenericContainer(const MethodInfo *method) {
+        if (!method->is_generic) {
+            SAFE_ABORT("METHOD IS NOT GENERIC");
+        }
+
+        // pyrocynical lol
+        if (method->is_inflated) {
+#if UNITY_2021
+            return reinterpret_cast<const Il2CppGenericContainer *>(method->genericMethod->methodDefinition->genericContainerHandle);
+#else // UNITY_2021
+            return method->genericMethod->methodDefinition->genericContainer;
+#endif // UNITY_2021
+        }
+
+#if UNITY_2021
+        return reinterpret_cast<const Il2CppGenericContainer *>(method->genericContainerHandle);
+#else // UNITY_2021
+            return method->genericContainer;
+#endif // UNITy_2021
+    }
 
     GLUON_API const MethodInfo *resolveMethodWithSlot(Il2CppClass *klass, std::uint16_t slot) noexcept;
     GLUON_API const MethodInfo *resolveVTableSlot(Il2CppClass *klass, Il2CppClass *declaringClass, std::uint16_t slot) noexcept;
@@ -22,7 +167,112 @@ namespace Gluon::Methods {
     template <typename T, typename... TArgs>
     requires(!std::is_same_v<T, Il2CppClass *>)
     const MethodInfo *resolveVTableSlot(T &&instance, TArgs &&...args) noexcept {
-        return resolveVTableSlot(Gluon::Classes::extractClass(instance, args...));
+        return resolveVTableSlot(Gluon::Classes::extractClass(instance), args...);
+    }
+
+    /**
+     * Returns whether a given MethodInfo's parameters match the Il2CppType vector
+     * @param isIdenticalOut is true if every parameter type matches identically. Can be null
+     */
+    template <std::size_t genericsCount, std::size_t argsCount>
+    bool parameterMatch(const MethodInfo *method, const std::span<const Il2CppClass *const, genericsCount> genericTypes,
+                        const std::span<const Il2CppType *const, argsCount> argTypes,
+                        const std::optional<bool *> isIdenticalOut) {
+
+        Gluon::Il2CppFunctions::initialise();
+
+        if (method->parameters_count != argTypes.size()) {
+            Gluon::Logger::warn("Potential method had wrong number of parameters {} (expected {})", method->parameters_count, argTypes.size());
+            return false;
+        }
+
+        const Il2CppGenericContainer *genericContainer;
+
+        std::int32_t containerSize = 0;
+        if (method->is_generic) {
+            genericContainer = getGenericContainer(method);
+            containerSize = genericContainer->type_argc;
+        }
+
+        if (containerSize != genericTypes.size()) {
+            Gluon::Logger::warn("Potential method had wrong number of generics {} (expected {})", containerSize, genericTypes.size());
+            Gluon::Logger::warn("is generic: {} is inflated: {}", method->is_generic, method->is_inflated);
+            return false;
+        }
+
+        bool isIdentical = true;
+        bool matches = true;
+
+        for (std::uint8_t i = 0; i < method->parameters_count; i++) {
+            const Il2CppType *parameterType = method->parameters[i];
+
+            if (argTypes[i] == nullptr) {
+                Gluon::Logger::warn("Arg type {} is null. Method: {:x}", i, reinterpret_cast<std::uintptr_t>(method));
+                // TODO: Log method
+                continue;
+            }
+
+            if (parameterType->type == IL2CPP_TYPE_MVAR) {
+                if (containerSize == 0) {
+                    Gluon::Logger::warn("No generic arguments to extract parameter index {}", i);
+                    continue;
+                }
+
+                GenericParameterIndex genericParameterIndex =
+                    Gluon::Il2CppFunctions::MetadataCache_GetGenericParameterIndexFromParameter(parameterType->data.genericParameterHandle)
+                    // ReSharper disable once CppLocalVariableMightNotBeInitialized
+                    - genericContainer->genericParameterStart;
+
+                if (genericParameterIndex < 0) {
+                    Gluon::Logger::warn("Extracted invalid generic parameter index {} from parameter {}", genericParameterIndex, i);
+                    continue;
+                }
+
+                if (genericParameterIndex >= containerSize) {
+                    Gluon::Logger::warn(
+                        "parameterMatch was not supplied enough generic types to determine the type of parameter {}"
+                        "(had {}, needed {})",
+                        i, containerSize, genericParameterIndex);
+                    continue;
+                }
+
+                const Il2CppClass *klass = genericTypes[i];
+                parameterType = parameterType->byref ? &klass->this_arg : &klass->byval_arg;
+            }
+
+            // parameters are identical if every parameter matches exactly
+            isIdentical &= parameterType == argTypes[i];
+
+            if (!isConvertibleFrom(parameterType, argTypes[i])) {
+                matches = false;
+                break;
+            }
+        }
+
+        // write to out
+        if (isIdenticalOut.has_value()) {
+            *isIdenticalOut.value() = isIdentical;
+        }
+
+        return matches;
+    }
+
+    template <std::size_t argsCount>
+    bool parameterMatch(const MethodInfo *method, const std::span<const Il2CppType *const, argsCount> argTypes,
+                        const std::optional<bool *> isIdenticalOut) {
+        return parameterMatch<0, argsCount>(method, std::span<const Il2CppClass *const, 0>(), argTypes, isIdenticalOut);
+    }
+
+    template <bool strictEqual = false, std::size_t genericsCount, std::size_t argsCount>
+    bool parameterMatch(const MethodInfo *method, std::array<const Il2CppClass *, genericsCount> const &genericTypes,
+                        std::array<const Il2CppType *, argsCount> const &argTypes, std::optional<bool *> isIdenticalOut) {
+        return parameterMatch<genericsCount, argsCount>(method, genericTypes, argTypes, isIdenticalOut);
+    }
+
+    template <bool strictEqual = false, size_t argsCount>
+    bool parameterMatch(const MethodInfo *method, std::array<const Il2CppType *, argsCount> const &argTypes,
+                        std::optional<bool *> isIdenticalOut) {
+        return parameterMatch<0, argsCount>(method, std::span<const Il2CppClass *const, 0>(), argTypes, isIdenticalOut);
     }
 }
 
--- src/methods.cpp
+++ src/methods.cpp
@@ -2,14 +2,18 @@
 
 #include <unordered_map>
 #include <mutex>
+#include <shared_mutex>
 #include <span>
 #include <string>
+#include <sstream>
 
 #include "abortion.hpp"
 #include "arrays.hpp"
 #include "classes.hpp"
 #include "hashing.hpp"
 #include "il2cpp_functions.hpp"
+
+#include "il2cpp-tabledefs.h"
 
 namespace Gluon::Methods {
     const MethodInfo *findMethodUnsafe(const Il2CppClass *klass, std::string_view methodName, int argsCount) {
@@ -53,6 +57,275 @@ namespace Gluon::Methods {
         Gluon::Il2CppFunctions::initialise();
         const Il2CppClass *klass = RET_DEFAULT_UNLESS(Gluon::Il2CppFunctions::object_get_class(instance));
         return findMethodUnsafe(klass, methodName, argsCount);
+    }
+
+    // get class from type and match Var/MVar
+    Il2CppClass *getTypeClass(const Il2CppType *type, const MethodInfo *methodInfo) {
+        if (type->type == IL2CPP_TYPE_CLASS || type->type == IL2CPP_TYPE_VALUETYPE) {
+            return Gluon::Il2CppFunctions::MetadataCache_GetTypeInfoFromHandle(type->data.typeHandle);
+        }
+
+        if (type->type == IL2CPP_TYPE_VAR || type->type == IL2CPP_TYPE_MVAR) {
+            const GenericParameterIndex genericParameterIndex =
+                Gluon::Il2CppFunctions::MetadataCache_GetGenericParameterIndexFromParameter(type->data.genericParameterHandle);
+            const Il2CppGenericContainer *genericContainer = getGenericContainer(methodInfo);
+            const Il2CppGenericInst *genericInst = methodInfo->genericMethod->context.method_inst;;
+
+            const Il2CppType *genericType = genericInst->type_argv[genericParameterIndex - genericContainer->genericParameterStart];
+            return getTypeClass(genericType, methodInfo);
+        }
+
+        if (type->type == IL2CPP_TYPE_GENERICINST) {
+            const Il2CppType *genericType = type->data.generic_class->type;
+            return getTypeClass(genericType, methodInfo);
+        }
+
+        return nullptr;
+    }
+
+    std::int64_t calculateParameterWeight(Il2CppClass *methodParamClass, Il2CppClass *expectedParamClass) {
+        if (!methodParamClass || !expectedParamClass) {
+            return 1;
+        }
+
+        std::span<const Il2CppClass *const> methodInterfaces = { methodParamClass->implementedInterfaces, methodParamClass->interfaces_count };
+
+        std::int64_t distance = 0;
+
+        if (methodParamClass == expectedParamClass) {
+            return distance;
+        }
+
+        const bool isMethodInterface = methodParamClass->flags & TYPE_ATTRIBUTE_INTERFACE;
+
+        // method is an interface, we should STRONGLY prefer a concrete type - avoid at all costs
+        if (const bool isExpectedInterface = expectedParamClass->flags & TYPE_ATTRIBUTE_INTERFACE; isExpectedInterface && !isMethodInterface) {
+            return 1000;
+        }
+
+        if (isMethodInterface) {
+            // if interface, just add lots of weight
+            // prefer concrete types
+            distance += 5;
+        }
+
+        std::span<const Il2CppClass *const> expectedInterfaces =
+            { expectedParamClass->implementedInterfaces, expectedParamClass->interfaces_count };
+
+        // find all interface that intersect with our expected type
+        std::vector<const Il2CppClass *> interfaceIntersections;
+        interfaceIntersections.reserve(expectedInterfaces.size());
+        std::ranges::set_intersection(expectedInterfaces, methodInterfaces, std::back_inserter(interfaceIntersections));
+        const std::size_t interfaceSharing = interfaceIntersections.size();
+
+        while (expectedParamClass && expectedParamClass != methodParamClass) {
+            if (!Gluon::Il2CppFunctions::class_is_assignable_from(methodParamClass, expectedParamClass)) {
+                break;
+            }
+
+            expectedParamClass = expectedParamClass->parent;
+            distance++;
+        }
+
+        // subtract distance by specificity of interface, since it allows specificity
+        distance -= interfaceSharing;
+
+        return distance;
+    }
+
+    const MethodInfo *findMethod(FindMethodInfo &&info) {
+        static std::unordered_map<FindMethodInfo, const MethodInfo *> methodsCache;
+        static std::shared_mutex methodsCacheMutex;
+
+        Gluon::Il2CppFunctions::initialise();
+        Il2CppClass *klass = info.klass;
+        RET_DEFAULT_UNLESS(klass);
+
+        // check cache
+        {
+            std::shared_lock lock(methodsCacheMutex);
+            if (const auto itr = methodsCache.find(info); itr != methodsCache.end()) {
+                return itr->second;
+            }
+        }
+
+        // let me break it down for you, mark
+        //
+        // we look though all the methods have the following:
+        // - matching name
+        // - parameters match as defined in the parametersMatch function
+        //
+        // resolution priority is as follows:
+        // - Identical parameters
+        // - Only one method found
+        // - Multiple matches: calculate a weight for all methods and use the lowest weighed one
+        //
+        // Cache results
+
+        std::vector<const MethodInfo *> matches;
+        matches.reserve(1);
+
+        const MethodInfo *target;
+
+        auto addMethodsToMatches = [&](Il2CppClass *targetClass) {
+            // initialise class if needed
+            if (!targetClass->initialized_and_no_error) {
+                Gluon::Il2CppFunctions::Class_Init(targetClass);
+            }
+
+            // does NOT automatically recurse through the target's parents
+            for (const auto methods = std::span(targetClass->methods, targetClass->method_count); const MethodInfo *const &current : methods) {
+                if (info.name != current->name) {
+                    continue;
+                }
+
+                // strict equality
+                bool isPerfect;
+                if (!parameterMatch(current, std::span(info.genericTypes), std::span(info.argTypes), &isPerfect)) {
+                    continue;
+                }
+
+                if (isPerfect) {
+                    target = current;
+                    break;
+                }
+
+                // I have no clue why this is here, its unused but im too coward to remove it
+                auto methodParameters = std::span(current->parameters, current->parameters_count);
+                matches.push_back(current);
+            }
+        };
+
+        // now look for matches recursively
+        Il2CppClass *targetClass = info.klass;
+        // if we reached no parent or prefect target is found, finish up
+        while (targetClass != nullptr && target == nullptr) {
+            addMethodsToMatches(targetClass);
+            targetClass = targetClass->parent;
+        }
+
+        // Resolve method overloads
+        if (!target && !matches.empty()) {
+            // Only found one method? it's gonna have to do
+            if (matches.size() == 1) {
+                target = matches.front();
+            }
+
+            // multiple methods found
+            // resolve overloads
+            if (!target) {
+                std::vector<std::pair<const MethodInfo *, std::int64_t>> weights;
+                weights.reserve(matches.size());
+
+                // iterate over methods
+                for (const MethodInfo *const &method: matches) {
+                    // overload resolution
+                    std::int64_t weight = 0;
+
+                    // weight the methods based on how far away they are from the expected parameters
+                    for (std::size_t i = 0; i < info.argTypes.size(); i++) {
+                        const Il2CppType *const &expectedParamType = info.argTypes[i];
+                        const Il2CppType *const &methodParamType = method->parameters[i];
+
+                        Il2CppClass *const methodParamClass = getTypeClass(methodParamType, method);
+                        Il2CppClass *const expectedParamClass = getTypeClass(expectedParamType, method);
+
+                        weight += calculateParameterWeight(methodParamClass, expectedParamClass);
+                    }
+
+                    weights.emplace_back(method, weight);
+                }
+
+                if (!target) {
+                    Gluon::Logger::warn("findMethod: Found multiple methods that match for {}.{}",
+                                        Gluon::Classes::getClassStandardName(klass).c_str(), info.name.data());
+                    for (auto const &[method, weight] : weights) {
+                        Gluon::Logger::warn("findMethod: weight {} method {}", weight, method->name);
+                        // TODO: Log method
+                    }
+
+                    target = std::ranges::min_element(weights,
+                        [](const std::pair<const MethodInfo *, std::int64_t> &a,
+                                const std::pair<const MethodInfo *, std::int64_t> &b) {
+                        return a.second < b.second;
+                    })->first;
+
+                    CRASH_UNLESS(target);
+
+                    // Gluon::Logger::warn("Using the following:");
+                    // TODO: Log method
+                }
+            }
+        }
+
+        if (!target) {
+            std::stringstream ss;
+            ss << (matches.size() > 1 ? "found multiple matches for" : "could not find");
+            ss << " method " << info.name;
+
+            bool first = true;
+            // list generic types
+            if (!info.genericTypes.empty()) {
+                ss << "<";
+                for (const Il2CppClass *genericType : info.genericTypes) {
+                    if (!first) {
+                        ss << ", ";
+                    }
+
+                    first = false;
+                    ss << Gluon::Classes::getClassStandardName(genericType);
+                }
+                ss << ">";
+            }
+
+            first = true;
+            ss << "(";
+            for (const Il2CppType *argType : info.argTypes) {
+                if (first) {
+                    ss << ", ";
+                }
+
+                first = false;
+                ss << Gluon::Classes::getTypeSimpleName(argType);
+            }
+
+            ss << ") in class '" << Gluon::Classes::getClassStandardName(klass) << "'";
+            Gluon::Logger::error("{}", ss.str().c_str());
+            // TODO: Log methods
+        }
+
+        // add to cache
+        {
+            std::unique_lock lock(methodsCacheMutex);
+            methodsCache.emplace(std::move(info), target);
+        }
+
+        return target;
+    }
+
+    bool isConvertibleFrom(const Il2CppType *to, const Il2CppType *from, bool asArgs) {
+        RET_0_UNLESS(to);
+        RET_0_UNLESS(from);
+
+        Gluon::Il2CppFunctions::initialise();
+
+        if (asArgs && to->byref && !from->byref) {
+            Gluon::Logger::debug("to ({}, {:x}) is ref/out while from  ({}, {:x}) is not. Not convertible.",
+                Gluon::Classes::getTypeSimpleName(to), reinterpret_cast<std::uintptr_t>(to),
+                Gluon::Classes::getTypeSimpleName(from), reinterpret_cast<std::uintptr_t>(from));
+            return false;
+        }
+
+        Il2CppClass *classTo = Gluon::Il2CppFunctions::class_from_il2cpp_type(to);
+        Il2CppClass *classFrom = Gluon::Il2CppFunctions::class_from_il2cpp_type(from);
+
+        bool ret = to->type == IL2CPP_TYPE_MVAR || Gluon::Il2CppFunctions::class_is_assignable_from(classTo, classFrom);
+
+        if (!ret && Gluon::Il2CppFunctions::class_is_enum(classTo)) {
+            ret = isConvertibleFrom(Gluon::Il2CppFunctions::class_enum_basetype(classTo), from, asArgs);
+        }
+
+        return ret;
     }
 
     const MethodInfo *resolveMethodWithSlot(Il2CppClass *klass, std::uint16_t slot) noexcept {
@@ -121,7 +394,7 @@ namespace Gluon::Methods {
             Gluon::Logger::warn("Looking for: {}, resolved to: {}", slot, method->slot);
 
             method = resolveMethodWithSlot(klass, slot);
-            Gluon::Logger::info("After resolving method with slot: {} found method {}", slot, reinterpret_cast<std::uintptr_t>(method));
+            Gluon::Logger::info("After resolving method with slot: {} found method {:x}", slot, reinterpret_cast<std::uintptr_t>(method));
         }
 
         // resolved method slot should be the slot we asked for if it came from a non-interface
