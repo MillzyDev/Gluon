146cccbb9304de14bd3d05c314500d74af6a3623
Millz <millzyyt08@gmail.com>
2025-06-03 20:19:50 +0100

    array

---
+++ include/wrappers_object.hpp
@@ -0,0 +1,11 @@
+#ifndef GLUON_WRAPPERS_OBJECT_HPP_
+#define GLUON_WRAPPERS_OBJECT_HPP_
+
+#include "type_concepts.hpp"
+
+#include "il2cpp-config.h"
+#include "il2cpp-object-internals.h"
+
+MARK_REF_PTR_T(Il2CppObject);
+
+#endif // GLUON_WRAPPERS_OBJECT_HPP_
