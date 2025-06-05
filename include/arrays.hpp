4e7cd4af8e76250be912d9d54e9f0b69459e9220
Millz <millzyyt08@gmail.com>
2025-06-05 17:21:07 +0100

    missing array stuff

--- include/arrays.hpp
+++ include/arrays.hpp
@@ -2,8 +2,21 @@
 #define GLUON_ARRAYS_HPP_
 
 #include <cstdint>
+#include <memory>
 
 #include "il2cpp-object-internals.h"
+
+#define LOAD_ARRAY_ELEMENTS(array, index, size) ((reinterpret_cast<std::uint8_t>(array) + kIl2CppSizeOfArray) + ((size) * (index)))
+
+#define IL2CPP_ARRAY_SET_WITH_SIZE(array, elementSize, index, value) {                                          \
+    void *ptr = reinterpret_cast<void *>(Gluon::Arrays::il2cppArrayAddrWithSize(array, elementSize, (index));   \
+    std::memcpy(ptr, &(value), elementSize);                                                                    \
+}
+
+#define IL2CPP_ARRAY_SET_REF_WITH_SIZE(array, elementSize, index, value) {                                      \
+    void *ptr = reinterpret_cast<void *>(Gluon::Arrays::il2cppArrayAddrWithSize(array, elementSize, (index));   \
+    std::memcpy(ptr, value, elementSize);                                                                       \
+}
 
 #define IL2CPP_ARRAY_ADDR(array, type, index) reinterpret_cast<type *>(Gluon::Arrays::il2cppArrayAddrWithSize(array, sizeof(type), index))
 
