9eb199569426e40412f8943561ebad85eba5706d
millz <f.w.b.mills@gmail.com>
2025-11-23 20:38:20 +0000

    currently unused tracers

---
+++ include/tracers.hpp
@@ -0,0 +1,79 @@
+#ifndef GLUON_TRACERS_HPP_
+#define GLUON_TRACERS_HPP_
+
+#include <cstdint>
+#include <optional>
+
+#include "gluon_config.hpp"
+#include "gluon_logging.hpp"
+#include "capstone/capstone.h"
+
+namespace Gluon::Tracers {
+    GLUON_API void initCapstone();
+    GLUON_API csh getCapstone();
+
+    template <std::uint32_t N, x86_insn Instruction, std::uint8_t Operand>
+    GLUON_HIDDEN std::uint32_t *findNth(const std::uint32_t *address) {
+        cs_insn *insn = cs_malloc(getCapstone());
+        auto ptr = reinterpret_cast<std::uint64_t>(address);
+        auto instructions = reinterpret_cast<const std::uint8_t *>(address);
+
+        constexpr std::size_t kSize = 4096;
+        std::size_t size = kSize;
+        std::uint32_t n = N;
+
+        while (cs_disasm_iter(getCapstone(), &instructions, &size, &ptr, insn)) {
+            if (insn->id == Instruction) {
+                n -= 1;
+            }
+
+            if (n == 0) {
+                const cs_detail *detail = insn->detail;
+                const cs_x86 arch = detail->x86;
+
+                switch (const cs_x86_op operand = arch.operands[Operand]; operand.type) {
+                    case X86_OP_IMM:
+                        return reinterpret_cast<std::uint32_t *>(operand.imm);
+                    case X86_OP_MEM:
+                        if (operand.mem.base != X86_REG_RIP) {
+                            Gluon::Logger::warn("Instruction targeted at 0x{:x} (n={}, target={}) with MEM operand at index {} is not relative to Instruction Pointer.", insn->address, N, cs_insn_name(getCapstone(), Instruction), Operand);
+                            break;
+                        }
+
+                        return reinterpret_cast<std::uint32_t *>(insn->address + insn->size + operand.mem.disp);
+                    default:
+                        Gluon::Logger::warn("Instruction targeted at 0x{:x} (n={}, target={}) cannot be decoded; operand at index {} is not immediate nor a memory address.", insn->address, N, cs_insn_name(getCapstone(), Instruction), Operand);
+                        break;
+                }
+
+                break;
+            }
+        }
+
+        Gluon::Logger::warn("Could not find {} instruction (n={}) in the available buffer.", cs_insn_name(getCapstone(), Instruction), N);
+        cs_free(insn, 1);
+        return nullptr;
+    }
+
+    template <std::uint32_t N>
+    GLUON_HIDDEN std::uint32_t *findNthCall(const std::uint32_t *address) {
+        return findNth<N, X86_INS_CALL, 0>(address);
+    }
+
+    template <std::uint32_t N>
+    GLUON_HIDDEN std::uint32_t *findNthJmp(const std::uint32_t *address) {
+        return findNth<N, X86_INS_JMP, 0>(address);
+    }
+
+    template <std::uint32_t N>
+    GLUON_HIDDEN std::uint32_t *findNthMov(const std::uint32_t *address) {
+        return findNth<N, X86_INS_MOV, 1>(address);
+    }
+
+    template <std::uint32_t N>
+    GLUON_HIDDEN std::uint32_t *findNthLea(const std::uint32_t *address) {
+        return findNth<N, X86_INS_LEA, 1>(address);
+    }
+} // Gluon::Tracers
+
+#endif // GLUON_TRACERS_HPP_
---
+++ src/tracers.cpp
@@ -0,0 +1,22 @@
+#include "tracers.hpp"
+
+#include "abortion.hpp"
+#include "gluon_logging.hpp"
+
+namespace Gluon::Tracers {
+    static csh capstoneHandle;
+
+    void initCapstone() {
+        const cs_err csError = cs_open(CS_ARCH_X86, CS_MODE_64, &capstoneHandle);
+        cs_option(capstoneHandle, CS_OPT_DETAIL, 1);
+        if (csError) {
+            Gluon::Logger::error("Capstone initialisation failed! {0}", static_cast<int>(csError));
+            SAFE_ABORT();
+        }
+        Gluon::Logger::info("Capstone initialised!");
+    }
+
+    csh getCapstone() {
+        return capstoneHandle;
+    }
+} // Gluon::Tracers
