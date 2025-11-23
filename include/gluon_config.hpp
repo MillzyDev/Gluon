16544821cf042efe02a7e4bdf7d4dbc0b192fe79
millz <f.w.b.mills@gmail.com>
2025-11-23 14:05:55 +0000

    windows shitass headers not properly defining stuff for clang

--- include/gluon_config.hpp
+++ include/gluon_config.hpp
@@ -1,34 +1,44 @@
 #ifndef GLUON_CONFIG_HPP_
 #define GLUON_CONFIG_HPP_
 
+#include <cstddef>
+
 #ifdef GLUON_EXPORT
 
 /*
  *  GLUON_API - Export and imports API functions (what it says on the tin)
  */
-#ifdef _WIN32
-#define GLUON_API __declspec(dllexport)
-#else
+//#ifdef _WIN32
+//#define GLUON_API __declspec(dllexport)
+//#else
 #define GLUON_API __attribute__((visibility("default")))
-#endif // _WIN32
+//#endif // _WIN32
 
 #else // GLUON_EXPORT
 
-#ifdef _WIN32
-#define GLUON_API __declspec(dllimport)
-#else // _WIN32
+// _WIN32
+//#define GLUON_API __declspec(dllimport)
+//#else // _WIN32
 #define GLUON_API
-#endif // _WIN32
+//#endif // _WIN32
 
 #endif // GLUON_EXPORT
 
 /*
  *  GLUON_HIDDEN - Never export, never import
  */
-#ifdef _WIN32
-#define GLUON_HIDDEN
-#else
+//#ifdef _WIN32
+//#define GLUON_HIDDEN
+//#else
 #define GLUON_HIDDEN __attribute__((visibility("hidden")))
-#endif // _WIN32
+//#endif // _WIN32
+
+#ifndef HANDLE
+#define HANDLE void *
+#endif // HANDLE
+
+#ifndef DWORD
+#define DWORD ::std::uint32_t
+#endif // DWORD
 
 #endif // GLUON_CONFIG_HPP_
