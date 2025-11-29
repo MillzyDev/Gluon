4d2cf0f8adc3f11d3b34dc2fed7ef49ce91b86eb
millz <f.w.b.mills@gmail.com>
2025-11-29 13:28:38 +0000

    export

--- include/assemblies.hpp
+++ include/assemblies.hpp
@@ -3,12 +3,13 @@
 
 #include <span>
 
+#include "gluon_config.hpp"
 #include "il2cpp_functions.hpp"
 
 #include "il2cpp-class-internals.h"
 
 namespace Gluon::Assemblies {
-    std::span<const Il2CppAssembly *> getAssemblies();
+    GLUON_API std::span<const Il2CppAssembly *> getAssemblies();
 
     inline const Il2CppImage *getImage(const Il2CppAssembly *assembly) {
         return Gluon::Il2CppFunctions::assembly_get_image(assembly);
